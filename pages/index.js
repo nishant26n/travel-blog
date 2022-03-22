f46ff099e1187f03331f4fcaeaa59dc84c7b6ebc
Nishant Rajpoot <nishant26n@Nishants-MacBook-Air.local>
2022-03-22 17:50:06 +0530

    created the components

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import { PostCard, PostWidget, Categories } from "../components";
 
 const posts = [
   { title: "Shimla Trip", excerpt: "Mall Road, Kufri and Green Valley" },
@@ -9,7 +8,7 @@ const posts = [
 
 export default function Home() {
   return (
-    <div className="container mx-auto px-10 mb-8 bg-gray-300">
+    <div className="container mx-auto px-10 mb-8">
       <Head>
         <title>Travel Blog</title>
         <link rel="icon" href="/favicon.ico" />
@@ -17,17 +16,17 @@ export default function Home() {
 
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post, index) => (
-            <div key="index">
-              {post.title}
-              {post.excerpt}
-            </div>
+          {posts.map((post) => (
+            <PostCard post={post} key={post.title} />
           ))}
         </div>
-      </div>
 
-      <div className="lg:col-span-4 col-span-1">
-        <div className="lg:sticky relative top-8"></div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
       </div>
     </div>
   );
