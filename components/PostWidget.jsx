f525e5c044913d6941683284571a8768d8bc4df5
Nishant Rajpoot <nishant26n@Nishants-MacBook-Air.local>
2022-03-24 19:03:21 +0530

    PostWidget component recent/similar posts displayed

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,7 +1,59 @@
-import React from "react";
+/* eslint-disable @next/next/no-img-element */
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+import Link from "next/link";
+import { getRecentPosts, getSimilarPosts } from "../services";
 
-const PostWidget = () => {
-  return <div>PostWidget</div>;
+const PostWidget = ({ categories, slug }) => {
+  const [relatedPosts, setRelatedPosts] = useState([]);
+
+  useEffect(() => {
+    if (slug) {
+      getSimilarPosts(categories, slug).then((result) =>
+        setRelatedPosts(result)
+      );
+    } else {
+      getRecentPosts(categories, slug).then((result) =>
+        setRelatedPosts(result)
+      );
+    }
+  }, [slug]);
+
+  console.log(relatedPosts);
+
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+        {slug ? "Related Posts" : "Recent Posts"}
+      </h3>
+      {relatedPosts.map((post) => (
+        <div key={post.title} className="flex items-center w-full mb-4">
+          <div className="w-16 flex-none">
+            <img
+              src={post.featuredImage.url}
+              alt={post.title}
+              className="align-middle rounded-full"
+              width="60px"
+              height="60px"
+            />
+          </div>
+
+          <div className="flex-grow ml-4">
+            <p className="text-gray-500 font-xs">
+              {moment(post.createdAt).format("MMM DD, YYYY")}
+            </p>
+            <Link
+              href={`/post/${post.slug}`}
+              key={post.title}
+              className="text-md"
+            >
+              {post.title}
+            </Link>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default PostWidget;
--- services/index.js
+++ services/index.js
@@ -36,3 +36,45 @@ export const getPosts = async () => {
 
   return result.postsConnection.edges;
 };
+
+export const getRecentPosts = async () => {
+  const query = `
+    query GetPostDetails() {
+      posts(
+        orderBy: createdAt_ASC
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
+};
+
+export const getSimilarPosts = async () => {
+  const query = `
+    query GetPostDetails($slug: String!, $categories: [String!]){
+      posts(
+        where : {slug_not: $slug, AND: {categories_some: {slug_in: $categories}}}
+        last: 3
+      ){
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
+};
