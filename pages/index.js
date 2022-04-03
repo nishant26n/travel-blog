fef56b5348d35f7ef18b29d393234223b1610bec
Nishant Rajpoot <nishant26n@Nishants-MacBook-Air.local>
2022-04-03 16:00:17 +0530

    FeaturedPost added

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { PostCard, PostWidget, Categories } from "../components";
+import { FeaturedPosts } from "../sections";
 import { getPosts } from "../services";
 
 export default function Home({ posts }) {
@@ -9,7 +10,7 @@ export default function Home({ posts }) {
         <title>Travel Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post) => (
