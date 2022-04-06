ac4a29532c11506ad99bbeb6f9b45012467c1ee3
Nishant Rajpoot <nishant26n@Nishants-MacBook-Air.local>
2022-04-06 22:18:34 +0530

    last commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import "../styles/globals.scss";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Layout } from "../components";
 
 function MyApp({ Component, pageProps }) {
