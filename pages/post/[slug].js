09500d7367c8f99c0d4637398a2707606876053a
Nishant Rajpoot <nishant26n@Nishants-MacBook-Air.local>
2022-04-16 15:28:17 +0530

    data fetching at build time

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -43,17 +43,20 @@ const PostDetails = ({ post }) => {
 
 export default PostDetails;
 
+// Fetch data at build time
 export async function getStaticProps({ params }) {
   const data = await getPostDetails(params.slug);
-
   return {
-    props: { post: data },
+    props: {
+      post: data,
+    },
   };
 }
 
+// Specify dynamic routes to pre-render pages based on data.
+// The HTML is generated at build time and will be reused on each request.
 export async function getStaticPaths() {
   const posts = await getPosts();
-
   return {
     paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
     fallback: true,
