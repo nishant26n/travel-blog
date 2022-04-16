dcaec6a7af2f8d41efd7872fca8a1a8b52bc2aa5
Nishant Rajpoot <nishant26n@Nishants-MacBook-Air.local>
2022-04-16 15:41:46 +0530

    last commit

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useRouter } from "next/router";
-import { getPosts, getPostDetails } from "../../services";
+
 import {
   PostDetail,
   Categories,
@@ -10,37 +10,39 @@ import {
   CommentsForm,
   Loader,
 } from "../../components";
+import { getPosts, getPostDetails } from "../../services";
 
 const PostDetails = ({ post }) => {
   const router = useRouter();
 
   if (router.isFallback) {
-    <Loader />;
+    return <Loader />;
   }
 
   return (
-    <div className="container mx-auto px-10 mb-8">
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        <div className="col-span-1 lg:col-span-8">
-          <PostDetail post={post} />
-          <Author author={post.author} />
-          <CommentsForm slug={post.slug} />
-          <Comments slug={post.slug} />
-        </div>
-        <div className="col-span-1 lg:col-span-4">
-          <div className="relative lg:sticky top-8">
-            <PostWidget
-              slug={post.slug}
-              categories={post.categories.map((category) => category.slug)}
-            />
-            <Categories />
+    <>
+      <div className="container mx-auto px-10 mb-8">
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+          <div className="col-span-1 lg:col-span-8">
+            <PostDetail post={post} />
+            <Author author={post.author} />
+            <CommentsForm slug={post.slug} />
+            <Comments slug={post.slug} />
+          </div>
+          <div className="col-span-1 lg:col-span-4">
+            <div className="relative lg:sticky top-8">
+              <PostWidget
+                slug={post.slug}
+                categories={post.categories.map((category) => category.slug)}
+              />
+              <Categories />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
-
 export default PostDetails;
 
 // Fetch data at build time
