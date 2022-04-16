6a3b060109776ec0ba75ac5c71d1289d1f2481de
Nishant Rajpoot <nishant26n@Nishants-MacBook-Air.local>
2022-04-16 19:59:21 +0530

    background

--- components/Header.jsx
+++ components/Header.jsx
@@ -15,17 +15,17 @@ const Header = () => {
   return (
     <div className="container mx-auto px-10 mb-10">
       <div className="border-b w-full inline-block border-blue-400 py-8">
-        <div className="md:float-left block ">
+        <div className="md:float-left block text-center font-mono">
           <Link href="/">
-            <span className="cursor-pointer font-bold text-4xl text-white">
-              Travel Blog
+            <span className="cursor-pointer font-bold text-2xl text-pink-900 lg:text-3xl">
+              Fearless Outing Blog
             </span>
           </Link>
         </div>
         <div className="hidden md:float-left  md:contents">
           {categories.map((category) => (
             <Link key={category.slug} href={`/category/${category.slug}`}>
-              <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer ">
+              <span className="md:float-right mt-2 align-middle text-pink-900 ml-4 font-semibold cursor-pointer ">
                 {category.name}
               </span>
             </Link>
